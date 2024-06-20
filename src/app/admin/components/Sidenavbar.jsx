e9ffc46601d8437c72dd9c23100212d4058c3094
lubabulmmm <lubabul.ilmi16@gmail.com>
2024-06-20 19:52:26 +0700

    transaksi and dashboard
    tabel transaksi & dashboard

--- src/app/admin/components/Sidenavbar.jsx
+++ src/app/admin/components/Sidenavbar.jsx
@@ -29,7 +29,7 @@ export default function Sidenavbar() {
       label: 'Produk',
     },
     {
-      id: '/admin/payment',
+      id: '/admin/transaksi',
       icon: <FaDollarSign />,
       label: 'Transaksi',
     },
